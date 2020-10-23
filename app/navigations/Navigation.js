65338bfd35f9937c0247baddec8ed4ad8017dfb1
hectoralarcon <hectoralarconguerra@gmail.com>
2020-10-23 16:38:53 -0500

    Las 5 pantallas en el menu

--- app/navigations/Navigation.js
+++ app/navigations/Navigation.js
@@ -24,6 +24,21 @@ export default function Navigation() {
           component={Favorites}
           options={{ title: "Favoritos" }}
         />
+        <Tab.Screen
+          name="top-restaurants"
+          component={TopRestaurants}
+          options={{ title: "Top 5" }}
+        />
+        <Tab.Screen
+          name="search"
+          component={Search}
+          options={{ title: "Buscar" }}
+        />
+        <Tab.Screen
+          name="account"
+          component={Account}
+          options={{ title: "Cuenta" }}
+        />
       </Tab.Navigator>
     </NavigationContainer>
   );
