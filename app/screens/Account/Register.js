47420f80863793fd8bbf59cd03967109aa340282
hectoralarcon <hectoralarconguerra@gmail.com>
2020-11-03 22:06:06 -0500

    RegisterForm

--- app/screens/Account/Register.js
+++ app/screens/Account/Register.js
@@ -1,9 +1,9 @@
 //import React, { useRef } from "react";
 import React from "react";
 import { StyleSheet, View, Text, Image } from "react-native";
-//import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
+import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
 //import Toast from "react-native-easy-toast";
-//import RegisterForm from "../../components/Account/RegisterForm";
+import RegisterForm from "../../components/Account/RegisterForm";
 
 export default function Register() {
   //const toastRef = useRef();
@@ -23,20 +23,27 @@ export default function Register() {
   }
 
   return (
-    <View>
-      <Text>Register...</Text>
-    </View>
+    <KeyboardAwareScrollView>
+      <Image
+        source={require("../../../assets/img/5-tenedores-letras-icono-logo.png")}
+        resizeMode="contain"
+        style={styles.logo}
+      />
+      <View style={styles.viewForm}>
+        <RegisterForm />
+      </View>
+    </KeyboardAwareScrollView>
   );
 }
 
 const styles = StyleSheet.create({
-  // logo: {
-  //  width: "100%",
-  //  height: 150,
-  //  marginTop: 20,
-  // },
-  // viewForm: {
-  //  marginRight: 40,
-  // marginLeft: 40,
-  // },
+  logo: {
+    width: "100%",
+    height: 150,
+    marginTop: 20,
+  },
+  viewForm: {
+    marginRight: 40,
+    marginLeft: 40,
+  },
 });
