d19209727609462a1beef6e27179992c898a7c85
hectoralarcon <hectoralarconguerra@gmail.com>
2020-11-05 16:42:56 -0500

    Login con firebase exitoso

--- app/components/Account/RegisterForm.js
+++ app/components/Account/RegisterForm.js
@@ -1,83 +1,17 @@
-import React from "react";
-//import React, { useState } from "react";
+import React, { useState } from "react";
 import { StyleSheet, View } from "react-native";
 import { Input, Icon, Button } from "react-native-elements";
 //import Loading from "../Loading";
-//import { validateEmail } from "../../utils/validations";
-//import { size, isEmpty } from "lodash";
-//import * as firebase from "firebase";
+import { validateEmail } from "../../utils/validations";
+import { size, isEmpty } from "lodash";
+import * as firebase from "firebase";
 //import { useNavigation } from "@react-navigation/native";
 
-export default function RegisterForm() {
-  return (
-    <View style={styles.formContainer}>
-      <Input
-        placeholder="Correo electronico"
-        containerStyle={styles.inputForm}
-        //     onChange={(e) => onChange(e, "email")}
-        rightIcon={
-          <Icon
-            type="material-community"
-            name="at"
-            iconStyle={styles.iconRight}
-          />
-        }
-      />
-      <Input
-        placeholder="Contraseña"
-        containerStyle={styles.inputForm}
-        password={true}
-        secureTextEntry={true}
-        //secureTextEntry={showPassword ? false : true}
-        // onChange={(e) => onChange(e, "password")}
-        rightIcon={
-          <Icon
-            type="material-community"
-            name="eye-outline"
-            //   name={showPassword ? "eye-off-outline" : "eye-outline"}
-            iconStyle={styles.iconRight}
-            //     onPress={() => setShowPassword(!showPassword)}
-          />
-        }
-      />
-      <Input
-        placeholder="Repetir contraseña"
-        containerStyle={styles.inputForm}
-        password={true}
-        secureTextEntry={true}
-        //secureTextEntry={showRepeatPassword ? false : true}
-        //  onChange={(e) => onChange(e, "repeatPassword")}
-        rightIcon={
-          <Icon
-            type="material-community"
-            //      name={showRepeatPassword ? "eye-off-outline" : "eye-outline"}
-            name="eye-outline"
-            iconStyle={styles.iconRight}
-            //      onPress={() => setShowRepeatPassword(!showRepeatPassword)}
-          />
-        }
-      />
-      <Button
-        title="Unirse"
-        containerStyle={styles.btnContainerRegister}
-        buttonStyle={styles.btnRegister}
-        //  onPress={onSubmit}
-      />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({});
-
-{
-  /*
 export default function RegisterForm(props) {
   const { toastRef } = props;
   const [showPassword, setShowPassword] = useState(false);
   const [showRepeatPassword, setShowRepeatPassword] = useState(false);
   const [formData, setFormData] = useState(defaultFormValue());
-  const [loading, setLoading] = useState(false);
-  const navigation = useNavigation();
 
   const onSubmit = () => {
     if (
@@ -95,6 +29,24 @@ export default function RegisterForm(props) {
         "La contraseña tiene que tener al menos 6 caracteres"
       );
     } else {
+      firebase
+        .auth()
+        .createUserWithEmailAndPassword(formData.email, formData.password)
+        .then((response) => {
+          console.log(response);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }
+    {
+      /*else if (formData.password !== formData.repeatPassword) {
+      //toastRef.current.show("Las contraseñas tienen que ser iguales");
+    } else if (size(formData.password) < 6) {
+      toastRef.current.show(
+        "La contraseña tiene que tener al menos 6 caracteres"
+      );
+    } else {
       setLoading(true);
       firebase
         .auth()
@@ -107,6 +59,7 @@ export default function RegisterForm(props) {
           setLoading(false);
           toastRef.current.show("El email ya esta en uso, pruebe con otro");
         });
+    }*/
     }
   };
 
@@ -164,7 +117,6 @@ export default function RegisterForm(props) {
         buttonStyle={styles.btnRegister}
         onPress={onSubmit}
       />
-      <Loading isVisible={loading} text="Creando cuenta" />
     </View>
   );
 }
@@ -176,7 +128,6 @@ function defaultFormValue() {
     repeatPassword: "",
   };
 }
-
 const styles = StyleSheet.create({
   formContainer: {
     flex: 1,
@@ -199,27 +150,3 @@ const styles = StyleSheet.create({
     color: "#c1c1c1",
   },
 });
-*/
-  const styles = StyleSheet.create({
-    formContainer: {
-      flex: 1,
-      alignItems: "center",
-      justifyContent: "center",
-      marginTop: 30,
-    },
-    inputForm: {
-      width: "100%",
-      marginTop: 20,
-    },
-    btnContainerRegister: {
-      marginTop: 20,
-      width: "95%",
-    },
-    btnRegister: {
-      backgroundColor: "#00a680",
-    },
-    iconRight: {
-      color: "#c1c1c1",
-    },
-  });
-}
--- app/screens/Account/Register.js
+++ app/screens/Account/Register.js
@@ -1,26 +1,11 @@
-//import React, { useRef } from "react";
-import React from "react";
-import { StyleSheet, View, Text, Image } from "react-native";
+import React, { useRef } from "react";
+import { StyleSheet, View, Image } from "react-native";
 import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
-//import Toast from "react-native-easy-toast";
+import Toast from "react-native-easy-toast";
 import RegisterForm from "../../components/Account/RegisterForm";
 
 export default function Register() {
-  //const toastRef = useRef();
-
-  {
-    /*    <KeyboardAwareScrollView>
-      <Image
-        source={require("../../../assets/img/5-tenedores-letras-icono-logo.png")}
-        resizeMode="contain"
-        style={styles.logo}
-      />
-      <View style={styles.viewForm}>
-        <RegisterForm toastRef={toastRef} />
-      </View>
-      <Toast ref={toastRef} position="center" opacity={0.9} />
-</KeyboardAwareScrollView>*/
-  }
+  const toastRef = useRef();
 
   return (
     <KeyboardAwareScrollView>
@@ -30,8 +15,9 @@ export default function Register() {
         style={styles.logo}
       />
       <View style={styles.viewForm}>
-        <RegisterForm />
+        <RegisterForm toastRef={toastRef} />
       </View>
+      <Toast ref={toastRef} position="center" opacity={0.9} />
     </KeyboardAwareScrollView>
   );
 }
