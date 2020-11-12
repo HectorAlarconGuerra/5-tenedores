69c4eb38d2ad89c2b5d2557dd2abb1e7eb69f624
hectoralarcon <hectoralarconguerra@gmail.com>
2020-11-12 00:23:17 -0500

    casi termiando password

--- app/components/Account/AccountOptions.js
+++ app/components/Account/AccountOptions.js
@@ -1,11 +1,11 @@
 import React, { useState } from "react";
-import { StyleSheet, View, Text } from "react-native";
+import { StyleSheet, View } from "react-native";
 import { ListItem } from "react-native-elements";
 import { map } from "lodash";
 import Modal from "../Modal";
 import ChangeDisplayNameForm from "./ChangeDisplayNameForm";
 import ChangeEmailForm from "./ChangeEmailForm";
-//import ChangePasswordForm from "./ChangePasswordForm";
+import ChangePasswordForm from "./ChangePasswordForm";
 
 export default function AccountOptions(props) {
   const { userInfo, toastRef, setRealoadUserInfo } = props;
@@ -38,8 +38,7 @@ export default function AccountOptions(props) {
         break;
       case "password":
         setRenderComponent(
-          <Text>Cambiando Contraseña</Text>
-          //     <ChangePasswordForm setShowModal={setShowModal} toastRef={toastRef} />
+          <ChangePasswordForm setShowModal={setShowModal} toastRef={toastRef} />
         );
         setShowModal(true);
         break;
